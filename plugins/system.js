703de194954654a2cb15acc65fca975694f294e7
mine-bbh <danupachithsara@gmail.com>
2024-09-30 18:43:36 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,1 +1,29 @@
+//==========system=======
+
+cmd({
+    pattern: "system",
+    react: "💦",
+    alias: ["device","bot","info"],
+    desc: "Get bot\'s system..",
+    category: "main",
+    use: '.system',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+  try{
+const tes = `┌───────────────────────
+├ 🧬 *Uptime:-*  ${runtime(process.uptime())}
+├ 🎲 *Ram usage:-*  ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+├ 🦄  *Owner:-* *DanuXzz ジ*
+├ 💠  *Version:-* 1.0.0
+└───────────────────────`
+
+ 
+await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: tes }, { quoted: mek, messageId:genMsgId() })
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
 
