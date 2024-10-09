cf43f29989acd43aa56fb0acd04b9eaa0c805ea6
mine-bbh <danupachithsara@gmail.com>
2024-10-09 16:23:47 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -117,7 +117,7 @@ cmd({
             }
         }
 
-        commandList += `\n⭓ *Total Commands in ${category}*: ${commands.filter(cmd => cmd.category.toUpperCase() === category).length}\n\n${wm}`
+        commandList += `\n✦ *Total Commands in ${category}*: ${commands.filter(cmd => cmd.category.toUpperCase() === category).length}\n\n${wm}`
 
         //await conn.sendMessage(from, { text: commandList }, { quoted: mek });
         await conn.sendMessage(from, {
