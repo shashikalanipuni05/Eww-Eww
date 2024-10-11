8292099838d0b2990541c9291ecf0ca8c4bc300a
mine-bbh <danupachithsara@gmail.com>
2024-10-11 20:22:39 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,6 +6,68 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 let cap = '*Qᴜᴇᴇɴ-ᴋᴇɴᴢɪ ᴍᴅ ᴠ2 ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ*\n*ᴄʀᴇᴀᴛᴇᴅ ʙʏ • ᴅᴀɴᴜxᴢᴢ*'
 //=====================================================================================
 cmd({
+        pattern: "alive",
+        react: "👻",
+        alias: ["online", "test", "bot"],
+        desc: "Check bot online or no.",
+        category: "other",
+        use: '.alive',
+        filename: __filename
+    },
+    async (conn, mek, m, {
+        from,
+        prefix,
+        pushname,
+        reply
+    }) => {
+        try {
+            if (os.hostname().length == 12) hostname = 'replit'
+            else if (os.hostname().length == 36) hostname = 'heroku'
+            else if (os.hostname().length == 8) hostname = 'koyeb'
+            else hostname = os.hostname()
+            let monspace = '```'
+            const sssf = `${monspace}💗 Hello ${pushname} I'm alive now${monspace}
+
+*😼ＫＥＮＺＩ-ＭＤ🤍*
+    
+> *ɴᴀᴍᴇ ʙᴏᴛ* : Qᴜᴇᴇɴ-ᴋᴇɴᴢɪ ᴍᴅ ᴠ2
+> *ᴠᴇʀsɪᴏɴs* : ${require("../package.json").version}
+> *ᴛʏᴘᴇ sᴄʀɪᴘᴛ* : ᴘʟᴜɢɪɴs
+> *Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+> *Runtime:* ${runtime(process.uptime())}
+> *Platform:* ${hostname}`
+
+            let buttons = [{
+                    name: "cta_url",
+                    buttonParamsJson: JSON.stringify({
+                        display_text: config.BTN,
+                        url: config.BTNURL,
+                        merchant_url: config.BTNURL
+                    }),
+                },
+                {
+                    name: "quick_reply",
+                    buttonParamsJson: JSON.stringify({
+                        display_text: "Get Menu",
+                        id: ".menu"
+                    }),
+                }
+            ]
+            let opts = {
+                image: config.LOGO,
+                header: '',
+                footer: config.FOOTER,
+                body: sssf
+
+            }
+            return await conn.sendButtonMessage(from, buttons, m, opts)
+        } catch (e) {
+            reply('*Error !!*')
+            console.log(e)
+        }
+    })
+
+cmd({
     pattern: "menu",
     react: "📜",
     alias: ["panel", "list", "commands", "cmd"],
