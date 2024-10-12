9ae84deb9543c093f8a690a1724ae28443fc7ab5
mine-bbh <danupachithsara@gmail.com>
2024-10-12 11:19:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ ALWAYS_ONLINE: process.env.ADDRESSES === undefined ? 'false' : process.env.ADDRE
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
 AUTO_RECORDING: process.env.AUTO_RECORDING === undefined ? 'true' : process.env.AUTO_RECORDING,
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/08ce20a843b7650a296e7.jpg",
 ALIVE: process.env.ALIVE === undefined ? `Hello im alive now !!` : process.env.ALIVE,
 OWNER: process.env.OWNER === undefined ? `94774500937` : process.env.OWNER,
 FOOTER: process.env.FOOTER=== undefined ? 'Qᴜᴇᴇɴ-ᴋᴇɴᴢɪ ᴍᴅ ᴠ2': process.env.FOOTER,
