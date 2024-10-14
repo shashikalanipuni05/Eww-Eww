1f0c3d8fb6d3ab532d89973e75736edd157f8d54
mine-bbh <danupachithsara@gmail.com>
2024-10-14 13:19:22 +0530

    Update imgdownload.js

--- plugins/imgdownload.js
+++ plugins/imgdownload.js
@@ -10,6 +10,7 @@ cmd({
     desc: "Search and send images from Google.",
     react: "🖼️",
     category: "download",
+    use: '.img1',
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
